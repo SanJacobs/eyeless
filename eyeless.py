96b72d8acdbb0c20e2a6ffa3387b841bbc799f35
San Jacobs <SanJacobs@protonmail.com>
2021-03-09 01:01:54 +0100

    Partially implemented folder verification

--- eyeless.py
+++ eyeless.py
@@ -14,7 +14,11 @@ def affirmatrue(input_string):
 input_argument_format = "eyeless.py face_input_dir render_output_dir (left)eye_input_dir (right_eye_input_dir)"
 # Reading input arguments
 
+properly_argumented = False
+
 if len(sys.argv) == 4 or len(sys.argv) == 5:
+    properly_argumented = True
+    
     print("Reading input-folder argument...")
     input_directory = sys.argv[1]
     print(input_directory)
@@ -36,11 +40,14 @@ elif len(sys.argv) > 5 or (len(sys.argv) < 4 and len(sys.argv) > 1):
     print("Proper format:")
     print(input_argument_format)
 
-else:
-    print("Input filepaths.")
-    print("Where is the folder containing the frames of the face?")
-    input_directory = input()
-    print("")
+while not properly_argumented:
+    input_directory = ""
+    
+    while not os.path.isdir(input_directory):
+        print("Input filepaths.")
+        print("Where is the folder containing the frames of the face?")
+        input_directory = input()
+        print("")
     
     print("Where is the folder containing the eye animation?")
     left_eye_directory = input()
@@ -50,6 +57,7 @@ else:
     print("Where do you want the finished files to go?")
     output_directory = input()
     print("")
+    
 
 # TODO: Check if input arguments are true
 # TODO: Separate left and right eye graphics
